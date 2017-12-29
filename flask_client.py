09b5efe42eed2501c4cc283dd136c17de0cd2e87
goutham-nekkalapu <goutham.nekkalapu@gmail.com>
2017-12-29 10:14:00 -0800

    feat : added a new URL

--- flask_client.py
+++ flask_client.py
@@ -5,8 +5,8 @@ headers = {'Content-type':'application/json'}
 
 #imageurl = 'https://www.organicfacts.net/wp-content/uploads/2013/05/Apple4-700x525.jpg'
 #imageurl = 'http://pngimg.com/uploads/orange/orange_PNG780.png'
-imageurl = 'http://farm1.static.flickr.com/170/391919618_3f36cd757e.jpg' 
-
+#imageurl = 'http://farm1.static.flickr.com/170/391919618_3f36cd757e.jpg' 
+imageurl = 'http://farm3.static.flickr.com/2500/4038251210_1060c180b0.jpg'
 
 data = {'url':imageurl}
 
